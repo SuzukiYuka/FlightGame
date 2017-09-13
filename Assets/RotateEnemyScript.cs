1baaabaa6ed3d41c37b1fc1cb286bf63dbd1e6f6
Yuka Suzuki <suzukiyuka1218@gmail.com>
2017-09-14 08:54:21 +0900

    first commit

---
+++ Assets/RotateEnemyScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateEnemyScript : MonoBehaviour {
+	float speed = 1f;
+	// Use this for initialization
+	void Start () {
+		speed = Random.Range (-10.0f, 10.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (0, 0, speed));
+		Debug.Log (speed);
+	}
+}
---
+++ Assets/RunEnemyScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunEnemyScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += new Vector3 (0, 0, -10.0f * Time.deltaTime);
+	}
+}
